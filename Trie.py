87c4afeed4afc085854c8eaa79a7af73b21f3255
Jason Liu <yordlejason@berkeley.edu>
2019-02-09 02:57:03 -0800

    removed test function

--- Trie.py
+++ Trie.py
@@ -32,15 +32,3 @@ class Trie:
                 return False
             node = node[c]
         return node[True]
-
-
-    def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts wnodeh the given prefix.
-        """
-        node = self._root
-        for c in prefix:
-            if not node[c]:
-                return False
-            node = node[c]
-        return True
