756f14ec350eef1e73151100db7af6bb72c54b64
Jason Liu <yordlejason@berkeley.edu>
2019-02-09 01:30:50 -0800

    - completed basic Trie implementation

---
+++ Trie.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+
+
+class Trie:
+
+    def __init__(self):
+        self.node = lambda: defaultdict(lambda: False)
+        self._root = self.node()
+        self._root[None] = True
+
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self._root
+        for c in word:
+            if not node[c]:
+                node[c] = self.node()
+                node[None] = True
+            node = node[c]
+        node[True] = True
+
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self._root
+        for c in word:
+            if not node[c]:
+                return False
+            node = node[c]
+        return node[True]
+
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts wnodeh the given prefix.
+        """
+        node = self._root
+        for c in prefix:
+            if not node[c]:
+                return False
+            node = node[c]
+        return True
